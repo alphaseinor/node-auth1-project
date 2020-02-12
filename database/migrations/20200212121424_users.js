a0f10f238a0f2ca40553571aee0c33b3626e7190
Brian Hague <alphaseinor@gmail.com>
2020-02-12 12:44:35 -0600

    added bridge table to migrations so more than one role can be assigned to user and more than one user can be assigned to roles

--- database/migrations/20200212121424_users.js
+++ database/migrations/20200212121424_users.js
@@ -1,6 +1,7 @@
 
 exports.up = function(knex) {
   return knex.schema
+    //for authX
     .createTable('users', users => {
       users.increments()
       users
@@ -14,7 +15,8 @@ exports.up = function(knex) {
         .boolean('active')
         .defaultTo(true)
     })
-    .createTable('roles', roles =>{
+    //for authZ
+    .createTable('roles', roles => {
       roles.increments()
       roles
         .string('name', 128)
@@ -26,8 +28,16 @@ exports.up = function(knex) {
       roles
         .boolean('active')
         .defaultTo(true)
-      roles
-        .integer('user_id')
+    })
+    //for authZ many users can have many roles, and many roles can be assigned to many users
+    .createTable('user_role', user_roles => {
+      user_roles
+        .primary('user_id', 'role_id')
+      user_roles.integer('role_id')
+        .notNullable()
+        .unsigned()
+        .references('roles.id')
+      user_roles.integer('user_id')
         .notNullable()
         .unsigned()
         .references('users.id')
