00ed36e1aea6d1496f0c256efde37842c0beb669
Brian Hague <alphaseinor@gmail.com>
2020-02-12 12:26:40 -0600

    added migration for users and roles

---
+++ database/migrations/20200212121424_users.js
@@ -0,0 +1,43 @@
+
+exports.up = function(knex) {
+  return ( 
+    knex.schema.createTable('users', users => {
+      users.increments()
+      users
+        .string('username', 128)
+        .notNullable()
+        .unique()
+      users
+        .string('password', 128)
+        .notNullable()
+      users
+        .boolean('active')
+        .defaultTo(true)
+    }),
+    knex.schema.createTable('roles', roles =>{
+      roles.increments()
+      roles
+        .string('name', 128)
+        .notNullable()
+        .unique()
+      roles
+        .string('description', 256)
+        .notNullable()
+      roles
+        .boolean('active')
+        .defaultTo(true)
+      roles
+        .integer('user_id')
+        .notNullable()
+        .unsigned()
+        .references('users.id')
+    })
+  )
+}
+
+exports.down = function(knex) {
+  return (
+    knex.schema.dropTableIfExists('users'),
+    knex.schema.dropTableIfExists('users')
+  )
+}
